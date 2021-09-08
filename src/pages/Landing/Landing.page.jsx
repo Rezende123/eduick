89cc02f6c03834d36753fe968713fcb61425aed1
Rezende123 <feliperezende.srz@gmail.com>
2021-09-08 13:22:37 -0300

    feat: Cria escopo do componente

--- src/pages/Landing/Landing.page.jsx
+++ src/pages/Landing/Landing.page.jsx
@@ -1,11 +1,16 @@
 import style from './Landing.module.scss'
 import Header from './components/Header'
+import FindBestTeacher from './components/FindBestTeacher'
 
 export default function Landing() {
   return (
     <div className={style.content}>
       <div className={style.content__header}>
         <Header></Header>
+      </div>
+
+      <div className={style.content__main}>
+        <FindBestTeacher></FindBestTeacher>
       </div>
       
       <div className={style.content__footer}></div>
