67ade46b1e7eea48246ca061815d7cd91798128f
Rezende123 <feliperezende.srz@gmail.com>
2021-09-10 20:55:50 -0300

    refactor: Corrige espaçamento das linhas

--- src/pages/Landing/components/MenuModal/MenuModal.component.jsx
+++ src/pages/Landing/components/MenuModal/MenuModal.component.jsx
@@ -7,44 +7,44 @@ import hamburger from '../../../../../public/icons/hamburger.svg'
 import { useState } from 'react'
 
 export default function MenuModal() {
-    Modal.setAppElement('section')
+  Modal.setAppElement('section')
 
-    const hideMenuModal = () => {
-        setShow(!show)
-    }
+  const hideMenuModal = () => {
+    setShow(!show)
+  }
 
-    const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false)
 
-    return (
-        <div>
-            <div onClick={hideMenuModal}>
-                <Image src={hamburger}/>
-            </div>
-            <Modal className={`${style["modal-container"]} ${style["custom-map-modal"]}`}
-                isOpen={show}
-                animation={true}
-            >
-                <section className={style.container}>
-                    <header className={style.header}>
-                        <figure className={style.header__logo}>
-                            <Image src={logo}/>
-                        </figure>
-                        <figure onClick={hideMenuModal}>
-                            <Image src={close}/>
-                        </figure>
-                    </header>
-                    <main className={style.menu}>
-                        <ul className={style["menu__list-pages"]}>
-                            <li>How it works</li>
-                            <li>About Us</li>
-                        </ul>
+  return (
+    <div>
+      <div onClick={hideMenuModal}>
+        <Image src={hamburger}/>
+      </div>
+      <Modal className={`${style["modal-container"]} ${style["custom-map-modal"]}`}
+        isOpen={show}
+        animation={true}
+      >
+        <section className={style.container}>
+          <header className={style.header}>
+            <figure className={style.header__logo}>
+              <Image src={logo}/>
+            </figure>
+            <figure onClick={hideMenuModal}>
+              <Image src={close}/>
+            </figure>
+          </header>
+          <main className={style.menu}>
+            <ul className={style["menu__list-pages"]}>
+              <li>How it works</li>
+              <li>About Us</li>
+            </ul>
 
-                        <button className={`landing_page_buttom landing_page_buttom--font ${style["menu__buttom"]}`}>
-                            Get Started
-                        </button>
-                    </main>
-                </section>
-            </Modal>
-        </div>
-    )
+            <button className={`landing_page_buttom landing_page_buttom--font ${style["menu__buttom"]}`}>
+              Get Started
+            </button>
+          </main>
+        </section>
+      </Modal>
+    </div>
+  )
 }
