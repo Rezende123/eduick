77a8d085fa9b235cacba0543d75eafa21b9c2706
Rezende123 <feliperezende.srz@gmail.com>
2021-09-10 12:10:23 -0300

    feat: Alinha a imagem de baixo com o footer para telas de tamanho menor

--- src/pages/Landing/Landing.page.jsx
+++ src/pages/Landing/Landing.page.jsx
@@ -4,6 +4,7 @@ import FindBestTeacher from './components/FindBestTeacher'
 import Image from 'next/image'
 import shape1 from '../../../public/images/group_1_descktop.svg'
 import shape2 from '../../../public/images/group_2.svg'
+import shape3 from '../../../public/images/group_3.png'
 
 export default function Landing() {
   return (
@@ -21,6 +22,9 @@ export default function Landing() {
         </figure>
         <figure id={style["shape-2"]}>
           <Image src={shape2} height="700"/>
+        </figure>
+        <figure id={style["shape-3"]}>
+          <Image src={shape3}/>
         </figure>
       </div>
       
