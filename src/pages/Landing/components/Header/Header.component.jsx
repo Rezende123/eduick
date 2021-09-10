3a6b5ebfbc56a00be61b12980a665da5222e3c0e
Rezende123 <feliperezende.srz@gmail.com>
2021-09-10 20:53:32 -0300

    feat: Exibe e esconde o modal

--- src/pages/Landing/components/Header/Header.component.jsx
+++ src/pages/Landing/components/Header/Header.component.jsx
@@ -1,23 +1,17 @@
 import style from './Header.module.scss'
 import Image from 'next/image'
 import logo from '../../../../../public/images/logo.svg'
-import hamburger from '../../../../../public/icons/hamburger.svg'
 import MenuModal from '../MenuModal'
 import { useState } from 'react';
 
 
 export default function Header() {
 
-  const [showMenuModal, setShowMenuModal] = useState(false)
-
-  const hideMenuModal = () => {
-    setShowMenuModal(!showMenuModal)
-  }
-
   return (
     <header className={style.header}>
-      <figure className={style.header__hamburger} onClick={hideMenuModal}>
-        <Image src={hamburger}/>
+      <figure className={style.header__hamburger}>
+        <MenuModal>
+        </MenuModal>
       </figure>
 
       <figure className={style.header__image}>
@@ -34,9 +28,6 @@ export default function Header() {
       <button className={`landing_page_buttom landing_page_buttom--font ${style.header__buttom}`}>
         Get Started
       </button>
-
-      <MenuModal show={showMenuModal}>
-      </MenuModal>
     </header>
 
   )
--- src/pages/Landing/components/MenuModal/MenuModal.component.jsx
+++ src/pages/Landing/components/MenuModal/MenuModal.component.jsx
@@ -1,37 +1,50 @@
-import Modal from "react-modal";
+import Modal from "react-modal"
 import Image from 'next/image'
 import style from './MenuModal.module.scss'
 import logo from '../../../../../public/images/logo.svg'
 import close from '../../../../../public/icons/close_big.svg'
+import hamburger from '../../../../../public/icons/hamburger.svg'
+import { useState } from 'react'
 
-export default function MenuModal(props) {
-    Modal.setAppElement('section');
+export default function MenuModal() {
+    Modal.setAppElement('section')
+
+    const hideMenuModal = () => {
+        setShow(!show)
+    }
+
+    const [show, setShow] = useState(false)
 
     return (
-        <Modal className={`${style["modal-container"]} ${style["custom-map-modal"]}`}
-            isOpen={props.show}
-            animation={true}
-        >
-            <section className={style.container}>
-                <header className={style.header}>
-                    <figure className={style.header__logo}>
-                        <Image src={logo}/>
-                    </figure>
-                    <figure>
-                        <Image src={close}/>
-                    </figure>
-                </header>
-                <main className={style.menu}>
-                    <ul className={style["menu__list-pages"]}>
-                        <li>How it works</li>
-                        <li>About Us</li>
-                    </ul>
+        <div>
+            <div onClick={hideMenuModal}>
+                <Image src={hamburger}/>
+            </div>
+            <Modal className={`${style["modal-container"]} ${style["custom-map-modal"]}`}
+                isOpen={show}
+                animation={true}
+            >
+                <section className={style.container}>
+                    <header className={style.header}>
+                        <figure className={style.header__logo}>
+                            <Image src={logo}/>
+                        </figure>
+                        <figure onClick={hideMenuModal}>
+                            <Image src={close}/>
+                        </figure>
+                    </header>
+                    <main className={style.menu}>
+                        <ul className={style["menu__list-pages"]}>
+                            <li>How it works</li>
+                            <li>About Us</li>
+                        </ul>
 
-                    <button className={`landing_page_buttom landing_page_buttom--font ${style["menu__buttom"]}`}>
-                        Get Started
-                    </button>
-                </main>
-            </section>
-        </Modal>
+                        <button className={`landing_page_buttom landing_page_buttom--font ${style["menu__buttom"]}`}>
+                            Get Started
+                        </button>
+                    </main>
+                </section>
+            </Modal>
+        </div>
     )
 }
