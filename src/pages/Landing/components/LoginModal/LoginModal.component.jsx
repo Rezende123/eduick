1d8632b9d1057fc9ff81645cf94f92233ae03e59
Rezende123 <feliperezende.srz@gmail.com>
2021-09-10 21:30:29 -0300

    feat: Define escopo do modal

--- src/pages/Landing/components/LoginModal/LoginModal.component.jsx
+++ src/pages/Landing/components/LoginModal/LoginModal.component.jsx
@@ -1,7 +1,29 @@
+import Modal from "react-modal"
 import LoginModal from './LoginModal.module.scss'
 
-export default function LoginModal() {
-    return (
-        <div></div>
-    )
+export default function LoginModal(props) {
+  Modal.setAppElement('section')
+
+  const [show, setShow] = useState(false)
+  const hideMenuModal = () => setShow(!show)
+
+  return (
+    <div>
+      <div onClick={hideMenuModal}>
+        {props.children}
+      </div>
+      <Modal
+        className={`${style["modal-container"]} ${style["custom-map-modal"]}`}
+        isOpen={show}
+        animation={true}
+      >
+        <section>
+          <header>
+          </header>
+          <main>
+          </main> 
+        </section>          
+      </Modal>
+    </div>
+  )
 }
